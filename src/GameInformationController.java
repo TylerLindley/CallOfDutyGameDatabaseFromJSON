fde54c2f9095aece4a93f07e63f9f32a7ef6231a
Tyler <200382154@student.georgianc.on.ca>
2020-12-03 21:18:00 -0500

    Populating the views with information and ListViews to be filled out with objects from the JSON file.

---
+++ src/GameInformationController.java
@@ -0,0 +1,25 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class GameInformationController {
+
+    @FXML
+    private Label gameTitleLabel;
+
+    @FXML
+    private Label publisherLabel;
+
+    @FXML
+    private Label releaseDateLabel;
+
+    @FXML
+    private Label totalRevenueLabel;
+
+    @FXML
+    private Label ratingLabel;
+
+    @FXML
+    private Button backButton;
+
+}
--- src/ListOfGamesController.java
+++ src/ListOfGamesController.java
@@ -1,2 +1,24 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class ListOfGamesController {
+
+    @FXML
+    private Label headerLabel;
+
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    private Label gamesFoundLabel;
+
+    @FXML
+    private ListView<?> databaseListView;
+
+    @FXML
+    private Button gameInfoButton;
+
 }
